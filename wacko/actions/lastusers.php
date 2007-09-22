6b589a8450410d66f74968fd7c6a77fbcbd635fa
Tann San <16375124+TannSan@users.noreply.github.com>
2007-09-22 14:35:52 +0000

    Fixed a bug with lastusers.php still using old quote() function

--- wacko/actions/lastusers.php
+++ wacko/actions/lastusers.php
@@ -8,10 +8,10 @@ if (!$max || $limit<$max)
 
 $last_users = $this->LoadAll("select name, signuptime from ".$this->config["user_table"]." order by signuptime desc limit ".(int)$max);
 
-foreach($last_users as $user) 
-{ 
- if ($stat!=="0") $num = $this->LoadSingle("select count(*) as n from ".$this->config["table_prefix"]."pages where owner='".quote($user["name"])."'");
- print("(<span class=\"dt\">".$user["signuptime"].")</span> ".$this->Link("/".$user["name"],"",$user["name"]).($stat!=="0"?" . . . (".$num["n"].")":"")."<br />\n"); 
+foreach($last_users as $user)
+{
+ if ($stat!=="0") $num = $this->LoadSingle("select count(*) as n from ".$this->config["table_prefix"]."pages where owner='".quote($this->dblink, $user["name"])."'");
+ print("(<span class=\"dt\">".$user["signuptime"].")</span> ".$this->Link("/".$user["name"],"",$user["name"]).($stat!=="0"?" . . . (".$num["n"].")":"")."<br />\n");
 }
 
-?> 
+?>
