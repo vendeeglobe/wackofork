a642fad4657727f7ac10e50fb7aab0971c73e481
vendeeglobe <54716082+vendeeglobe@users.noreply.github.com>
2007-10-15 05:07:36 +0000

    correct linking on subpages for mostpopular.php

--- wacko/actions/mostpopular.php
+++ wacko/actions/mostpopular.php
@@ -1,6 +1,6 @@
 <?php
 
-if (!$max)  $max = 25;
+if (!$max) $max = 25;
 if ($max>500) $max = 500;
 
 $pages = $this->LoadAll("select ".$this->pages_meta.",hits from ".$this->config["table_prefix"]."pages order by hits desc limit ".(2*(int)$max));
@@ -9,19 +9,19 @@ $num = 0;
 print("<table>");
 foreach ($pages as $page)
 {
-  if ($num<$max)
-  {
-   if ($this->config["hide_locked"]) $access = $this->HasAccess("read",$page["tag"]);
-   else $access = true;
-   if ($access)
-   {
-     // print entry
-     $num++;
-     print("<tr><td>&nbsp;&nbsp;".$num.".&nbsp;".$this->Link($page["tag"])."</td><td>".
-           $this->GetResourceValue("Shown")."</td><td>".
-           $page["hits"]."</td></tr>\n");
-   }
-  }
+if ($num<$max)
+{
+if ($this->config["hide_locked"]) $access = $this->HasAccess("read",$page["tag"]);
+else $access = true;
+if ($access)
+{
+// print entry
+$num++;
+print("<tr><td>  ".$num.". ".$this->Link("/".$page["tag"],"",$page["tag"])."</td><td>".
+$this->GetResourceValue("Shown")."</td><td>".
+$page["hits"]."</td></tr>\n");
+}
+}
 }
 print("</table>");
 
