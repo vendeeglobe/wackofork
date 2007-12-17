c99aa14e03c50a6f21e3d3ee8186ea482521fc2a
Tann San <16375124+TannSan@users.noreply.github.com>
2007-12-17 16:28:49 +0000

    Implemented Feature#102 - PageIndex by letter including result limiting.
    Added two arguments to the PageIndex action.  The first is "limit" which sets the maximum number of pages to show at any one time on each index page.  The second argument is "letter" which lets you pick which pages to display based on their name starting with a specific letter.

--- wacko/actions/pageindex.php
+++ wacko/actions/pageindex.php
@@ -1,89 +1,132 @@
 <?php
-//  action 'pageindex'
-//  modified by YaroslavSyuzev 02 Fev 2005
+
+/*
+   Page Index Action
+
+   {{pageindex
+      [limit="50"] // optional - number of pages to show at one time, if there are more pages then this the next/prev buttons are shown
+      [letter="a"] // optional - only display pages whose name starts with this letter
+   }}
+*/
 
 $offset = ( isset($_GET["offset"]) ) ? (int)$_GET["offset"] : 0;
-$limit = 100;
+if(!$limit) $limit = 10;
+if(isset($letter)) $letter = strtoupper(substr($letter, 0, 1));
 
-$sql = "select count(*) as n from ".$this->config["table_prefix"]."pages where ".
-       "latest = 'Y' and LEFT(supertag,7)!='comment'";
-$all_pages_count = $this->LoadSingle($sql);
-$total = $all_pages_count["n"];
+// Get tags for all the pages, even if they're not being displayed on this index page
+$sql = "select tag from ".$this->config["table_prefix"]."pages where latest = 'Y' and LEFT(supertag,7)!='comment' order by tag";
+$pages = $this->LoadAll($sql);
 
+$total = 0;
+$total_visible = 0;
+$top_links = "";
+$top_links_array = array();
+$page_links = "";
+$letter_count = 0;
+foreach($pages as $page)
+   {
+      $firstChar = strtoupper($page["tag"][0]);
+      if(!preg_match("/".$this->language["ALPHA"]."/", $firstChar)) { $firstChar = "#"; }
+
+      // Create alphabet links at top of page - Don't display this menu if the user specified a particluar letter
+      if($firstChar != $curChar)
+         {
+            $top_links_array[] = array("char" => $firstChar, "ind" => $total, "link" => $letter_count);
+
+            $oldChar = $curChar;
+            $curChar = $firstChar;
+            $letter_count++;
+         }
+
+      // Display the actual page link
+      if($this->config["hide_locked"]) $access = $this->HasAccess("read",$page["tag"]);
+      else $access = true;
+
+      if($access)
+         {
+            if($total >= $offset)
+               {
+                  if($total < $offset + $limit)
+                     {
+                        if(!$letter || $firstChar == $letter)
+                           {
+                              if($firstChar != $oldChar)
+                                 {
+                                    if ($oldChar && !$letter) $page_links.="<br />\n";
+                                    $page_links.="<a name=\"letter_".($letter_count - 1)."\"></a><strong>".$firstChar."</strong><br />\n";
+                                    $oldChar = $firstChar;
+                                 }
+
+                              $page_links.=$this->Link("/".$page["tag"],"",$page["tag"])."<br />\n";
+
+                              $total_visible++;
+                           }
+                     }
+               }
+
+            if(!$letter || $firstChar == $letter)
+               {
+                  $total++;
+               }
+         }
+  }
+
+// Display prev/next navigation links?
 if ($limit >= $total) $no_arr = true;
 
-//  collect data
+// Create the top links menu
+if(!$letter)
+   {
+      if(!$no_arr)
+         {
+            foreach($top_links_array as $link_data)
+               {
+                  $top_links.="<a href=\"?offset=".(floor($link_data["ind"] / $limit) * $limit)."#letter_".$link_data["link"]."\"><strong>".$link_data["char"]."</strong></a>\n";
+               }
+         }
+      else
+         {
+            foreach($top_links_array as $link_data)
+               {
+                  $top_links.="<a href=\"#letter_".$link_data["link"]."\"><strong>".$link_data["char"]."</strong></a>\n";
+               }
+         }
 
-$cnt = 0;
-$pages_to_display = array();
-$last_page = "";
+      print($top_links."<br /><br />\n");
+   }
 
-while ($cnt < $limit)
-{
-  $sql = "select ".$this->pages_meta." from ".$this->config["table_prefix"]."pages where ".
-         "latest = 'Y' and LEFT(supertag,7)!='comment' order by tag limit ".$offset.",".(2 * $limit);
+print($page_links);
 
-  if ($pages = $this->LoadAll($sql))
-  {
-    foreach ($pages as $page)
-    {
-      $offset++;
+if($page_links != "")
+   {
+      if(!$no_arr)
+         {
+            // Prev
+            if($offset + $total_visible > $limit)
+               {
+                  $prev_page_link = '<a href="?offset='.($offset - $limit > 0 ? $offset - $limit : 0).'">&lt; '.$this->GetResourceValue("Prev").'</a>';
+               }
+            else
+               {
+                  $prev_page_link = "&lt; ".$this->GetResourceValue("Prev");
+               }
 
-      if ($this->config["hide_locked"]) $access = $this->HasAccess("read",$page["tag"]);
-      else $access = true;
-      if ($access)
-      {
-          $pages_to_display[$page["tag"]] = $page;
-          $cnt++;
-      }
+            // Next
+            if($offset + $total_visible < $total)
+               {
+                  $next_page_link = '<a href="?offset='.($offset + $total_visible).'">'.$this->GetResourceValue("Next").' &gt;</a>';
+               }
+            else
+               {
+                  $next_page_link = $this->GetResourceValue("Next")." &gt;";
+               }
 
-      if ($cnt >= $limit) break 2;
-    }
-  }
-  else break;
-
-  if ($offset >= $total) break;
-}
-
-//  display collected data
-
-foreach ($pages_to_display as $page)
-{
-
-  $firstChar = strtoupper($page["tag"][0]);
-  if (!preg_match("/".$this->language["ALPHA"]."/", $firstChar)) { $firstChar = "#"; }
-
-  if ($firstChar != $curChar) {
-    if ($curChar) print("<br />\n");
-    print("<strong>$firstChar</strong><br />\n");
-    $curChar = $firstChar;
-  }
-
-  print($this->Link("/".$page["tag"],"",$page["tag"])."<br />\n");
-}
-
-//$last_page = array_pop($pages_to_display);
-
-//  display navigation
-
-if ( $offset < $total )
-  $next_page_link = '<a href="?offset='.$offset.'">'.$this->GetResourceValue("Next").' &gt;</a>';
+            print "<p class='logBtn'>$prev_page_link $next_page_link</p>\n";
+         }
+   }
 else
-  $next_page_link = $this->GetResourceValue("Next")." &gt;";
-
-if ($offset > $limit )
-  $prev_page_link = '<a href="?offset='.(($offset-2*$limit) > 0 ? $offset-2*$limit : 0).'">&lt; '.$this->GetResourceValue("Prev").'</a>';
-else
-  $prev_page_link = "&lt; ".$this->GetResourceValue("Prev");
-
-if ($pages_to_display)
-{
-  if (!$no_arr)
-  {
-    print "<p class='logBtn'>$prev_page_link $next_page_link</p>\n";
-  }
-}
-else
-  echo $this->GetResourceValue("NoPagesFound");
+   {
+      print $this->GetResourceValue("NoPagesFound");
+   }
 
 ?>
