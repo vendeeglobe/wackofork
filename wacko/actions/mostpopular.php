df13c7c9aa672f9e77da5fc2872e8b8d9ec2fc4c
Tann San <16375124+TannSan@users.noreply.github.com>
2007-12-19 20:42:10 +0000

    fixed dumbass bug in recursion and removed some none t-sql sql code that might of caused problems for certain pdo databases.

--- wacko/actions/mostpopular.php
+++ wacko/actions/mostpopular.php
@@ -27,15 +27,15 @@ else
             $for = substr($for, 1);
          }
 
-      if(!$dontrecurse || strtolower($dontrecurse) == 'true')
+      if(!$dontrecurse || strtolower($dontrecurse) == 'false')
          {
             // We want to recurse and include all the sub pages of sub pages (and so on) in the listing
-            $pages = $this->LoadAll("select tag, hits from ".$this->config["table_prefix"]."pages, ".$this->config["table_prefix"]."links WHERE INSTR(from_tag, '".$for."') = 1 AND tag = to_tag order by hits desc limit ".(2*(int)$max));
+            $pages = $this->LoadAll("select tag, hits from ".$this->config["table_prefix"]."pages, ".$this->config["table_prefix"]."links WHERE tag = to_tag AND INSTR(from_tag, '".$for."') = 1 AND INSTR(to_tag, '".$for."') = 1 order by hits desc limit ".(2*(int)$max));
          }
       else
          {
             // The only pages we want to display are those directly under the selected page, not their kids and grandkids
-            $pages = $this->LoadAll("select tag, hits from ".$this->config["table_prefix"]."pages, ".$this->config["table_prefix"]."links WHERE from_tag = '".$for."' AND tag = to_tag order by hits desc limit ".(2*(int)$max));
+            $pages = $this->LoadAll("select tag, hits from ".$this->config["table_prefix"]."pages, ".$this->config["table_prefix"]."links WHERE tag = to_tag AND from_tag = '".$for."' AND INSTR(to_tag, '".$for."') = 1 order by hits desc limit ".(2*(int)$max));
          }
    }
 
