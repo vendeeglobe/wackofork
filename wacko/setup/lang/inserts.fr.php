c676caf459936d30464388ca9e3dfa840f087d37
Didier Spaier <DidierSpaier@users.noreply.github.com>
2008-01-05 12:08:05 +0000

    Updated french translations and links in setup/lang/insert.fr.php and lang/wakka.fr.php

--- wacko/setup/lang/inserts.fr.php
+++ wacko/setup/lang/inserts.fr.php
@@ -3,7 +3,7 @@ $lng = "fr";
 
 if ($config["language"]==$lng)
 {
- InsertPage($config["root_page"], "((file:wacko4.gif WackoWiki))\n**Bienvenue sur votre site ((WackoWiki:WackoWiki WackoWiki))!**\n\nCliquez sur le lien \"Editer cette page\" en bas de la page pour commencer.\n\nUne documentation sommaire en anglais peut &ecirc;tre trouv&eacute;e ici : WackoWiki:DocFrancophone.\n\nPages utiles: PagesOrphelines, PagesDemandées, ((Recherche)), MesPages, MesModifications.\n\n", $lng);
+ InsertPage($config["root_page"], "((file:wacko4.gif WackoWiki))\n**Bienvenue sur votre site ((WackoWiki:WackoWiki WackoWiki))!**\n\nCliquez sur le lien \"Editer cette page\" en bas de la page pour commencer.\n\nUne documentation sommaire peut être trouvée ici : WackoWiki:DocFrancophone.\n\nPages utiles: PagesOrphelines, PagesDemandées, ((Recherche)), MesPages, MesModifications.\n\n", $lng);
  InsertPage('PagesDemandées', '{{WantedPages}}', $lng);
  InsertPage('PagesOrphelines', '{{OrphanedPages}}', $lng);
  InsertPage('MesPages', '{{MyPages}}', $lng);
@@ -12,11 +12,11 @@ if ($config["language"]==$lng)
 
 InsertPage('DernièresModifications', '{{RecentChanges}}', $lng);
 InsertPage('DerniersCommentaires', '{{RecentlyCommented}}', $lng);
-InsertPage('PageIndex', '{{PageIndex}}', $lng);
+InsertPage('Index', '{{PageIndex}}', $lng);
 InsertPage('Utilisateurs', '{{LastUsers}}', $lng);
 InsertPage('Enregistrement', '{{Registration}}', $lng);
 
-InsertPage('Password', '{{ChangePassword}}', $lng);
+InsertPage('MotDePasse', '{{ChangePassword}}', $lng);
 InsertPage('Recherche', '{{Search}}', $lng);
 InsertPage('Connexion', '{{Login}}', $lng);
 InsertPage('Préférences', '{{UserSettings}}', $lng);
